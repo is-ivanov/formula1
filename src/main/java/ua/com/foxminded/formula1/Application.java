12a5a2051d653e2aa30d5876dce6f7042761c0d0
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-18 22:37:10 +0300

    change pom

--- src/main/java/ua/com/foxminded/formula1/Application.java
+++ src/main/java/ua/com/foxminded/formula1/Application.java
@@ -3,13 +3,13 @@ package ua.com.foxminded.formula1;
 
 import ua.com.foxminded.formatter.Formattable;
 import ua.com.foxminded.formatter.Formatter;
-import ua.com.foxminded.parser.ParserResult;
+import ua.com.foxminded.parser.Parser;
 import ua.com.foxminded.parser.Parserable;
 import ua.com.foxminded.processor.Processor;
 
 public class Application {
     public static void main(String[] args) {
-        Parserable parser = new ParserResult();
+        Parserable parser = new Parser();
         Processor processor = new Processor();
         Formattable formatter = new Formatter();
         

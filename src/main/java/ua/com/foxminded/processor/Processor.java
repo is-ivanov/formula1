c05aae3f23b54ce33cea525cff2467a205943b64
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-19 23:19:36 +0300

    delete unused imports

--- src/main/java/ua/com/foxminded/processor/Processor.java
+++ src/main/java/ua/com/foxminded/processor/Processor.java
@@ -1,11 +1,7 @@
 package ua.com.foxminded.processor;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import ua.com.foxminded.racer.Racer;
 
 public class Processor {

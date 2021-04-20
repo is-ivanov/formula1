453e80517547be4a82746c9f917d58468d25b722
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-20 21:33:32 +0300

    add some tests to ParserTest

--- src/test/java/ua/com/foxminded/parser/ParserTest.java
+++ src/test/java/ua/com/foxminded/parser/ParserTest.java
@@ -3,6 +3,9 @@ package ua.com.foxminded.parser;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,12 +14,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import ua.com.foxminded.racer.Racer;
+
 @ExtendWith(MockitoExtension.class)
 class ParserTest {
     private static final String FILENAME_ABBREVIATION_FILE = "abbreviationsTest.txt";
     private static final String FILENAME_START_LOG = "startTest.log";
     private static final String FILENAME_END_LOG = "endTest.log";
-    private static final String FILENAME_IS_NULL = "Filename is null!!";
+    private static final String FILENAME_MISSING_FILE = "missingFile";
+    private static final String MESSAGE_FILENAME_IS_NULL = "Filename is null!!";
+    private static final String MESSAGE_FILE_NOT_FOUND = "File \"missingFile\" not found!!";
 
     Parserable parser;
 
@@ -35,7 +42,7 @@ class ParserTest {
             Exception exception = assertThrows(IllegalArgumentException.class,
                     () -> parser.parseDataFiles(null, FILENAME_START_LOG,
                             FILENAME_END_LOG));
-            assertEquals(FILENAME_IS_NULL, exception.getMessage());
+            assertEquals(MESSAGE_FILENAME_IS_NULL, exception.getMessage());
         }
 
         @Test
@@ -44,7 +51,7 @@ class ParserTest {
             Exception exception = assertThrows(IllegalArgumentException.class,
                     () -> parser.parseDataFiles(FILENAME_ABBREVIATION_FILE,
                             null, FILENAME_END_LOG));
-            assertEquals(FILENAME_IS_NULL, exception.getMessage());
+            assertEquals(MESSAGE_FILENAME_IS_NULL, exception.getMessage());
         }
 
         @Test
@@ -53,7 +60,7 @@ class ParserTest {
             Exception exception = assertThrows(IllegalArgumentException.class,
                     () -> parser.parseDataFiles(FILENAME_ABBREVIATION_FILE,
                             FILENAME_START_LOG, null));
-            assertEquals(FILENAME_IS_NULL, exception.getMessage());
+            assertEquals(MESSAGE_FILENAME_IS_NULL, exception.getMessage());
         }
 
     }
@@ -63,14 +70,46 @@ class ParserTest {
     class testMissingFile {
 
         @Test
-        @DisplayName("test input missing file shuold return IOException with message")
-        void testMissingAbbreviationFile() {
+        @DisplayName("test input missing abbreviations file should return IOException with message")
+        void testMissingAbbreviationsFile() {
             Exception exception = assertThrows(IOException.class,
-                    () -> parser.parseDataFiles("abbrevMiss",
+                    () -> parser.parseDataFiles(FILENAME_MISSING_FILE,
                             FILENAME_START_LOG, FILENAME_END_LOG));
-            assertEquals("", exception.getMessage());
+            assertEquals(MESSAGE_FILE_NOT_FOUND, exception.getMessage());
         }
 
+        @Test
+        @DisplayName("test input missing startLog file should return IOException with message")
+        void testMissingStartLogFile() {
+            Exception exception = assertThrows(IOException.class,
+                    () -> parser.parseDataFiles(FILENAME_ABBREVIATION_FILE,
+                            FILENAME_MISSING_FILE, FILENAME_END_LOG));
+            assertEquals(MESSAGE_FILE_NOT_FOUND, exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("test input missing endLog file should return IOException with message")
+        void testMissingEndLogFile() {
+            Exception exception = assertThrows(IOException.class,
+                    () -> parser.parseDataFiles(FILENAME_ABBREVIATION_FILE,
+                            FILENAME_START_LOG, FILENAME_MISSING_FILE));
+            assertEquals(MESSAGE_FILE_NOT_FOUND, exception.getMessage());
+        }
+
+    }
+
+    @Test
+    @DisplayName("test reading file with right data should return filled List of racers")
+    void testReadingRightData() throws Exception {
+        Racer drr = new Racer("DRR", "Daniel Ricciardo", "RED BULL RACING TAG HEUER");
+        drr.setStartTime(LocalDateTime.of(2018, 5, 24, 12, 14, 12, 054000000));
+        Racer svf = new Racer("SVF", "Sebastian Vettel", "FERRARI");
+        List<Racer> expectedRacers = new ArrayList<>();
+        expectedRacers.add(drr);
+        expectedRacers.add(svf);
+        List<Racer> actualRacers = parser.parseDataFiles(FILENAME_ABBREVIATION_FILE,
+                FILENAME_START_LOG, FILENAME_END_LOG);
+        assertEquals(expectedRacers, actualRacers);
     }
 
 }

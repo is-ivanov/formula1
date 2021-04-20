ace729f4f4aeda34e98551134969915a0844e266
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-20 21:55:02 +0300

    add methods hashCode and equals to Racer class for testing

--- src/main/java/ua/com/foxminded/racer/Racer.java
+++ src/main/java/ua/com/foxminded/racer/Racer.java
@@ -66,6 +66,62 @@ public class Racer {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lapTime == null) ? 0 : lapTime.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result
+                + ((startTime == null) ? 0 : startTime.hashCode());
+        result = prime * result + ((team == null) ? 0 : team.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Racer other = (Racer) obj;
+        if (endTime == null) {
+            if (other.endTime != null)
+                return false;
+        } else if (!endTime.equals(other.endTime))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (lapTime == null) {
+            if (other.lapTime != null)
+                return false;
+        } else if (!lapTime.equals(other.lapTime))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (startTime == null) {
+            if (other.startTime != null)
+                return false;
+        } else if (!startTime.equals(other.startTime))
+            return false;
+        if (team == null) {
+            if (other.team != null)
+                return false;
+        } else if (!team.equals(other.team))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "Racer [id=" + id + ", name=" + name + ", team=" + team
                 + ", startTime=" + startTime + ", endTime=" + endTime
